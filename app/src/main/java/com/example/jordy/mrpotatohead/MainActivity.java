d944c02f9245d642abda7ad060557455dab571b9
11433124 <jordy_hierck@live.nl>
2016-11-11 23:43:38 +0100

    Finally fixed layout in an empty hour

--- app/src/main/java/com/example/jordy/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/jordy/mrpotatohead/MainActivity.java
@@ -30,70 +30,70 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId())
         {
             // search if body part is clicked then set to visible and vice versa
-            case R.id.Eyes:
+            case R.id.eyes:
                 ImageView eyes = (ImageView) findViewById(R.id.eyespic);
                 if (((CheckBox) view).isChecked())
                     eyes.setVisibility(View.VISIBLE);
                 else
                     eyes.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Nose:
+            case R.id.nose:
                 ImageView nose = (ImageView) findViewById(R.id.nosepic);
                 if (((CheckBox) view).isChecked())
                     nose.setVisibility(View.VISIBLE);
                 else
                     nose.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Eyebrows:
+            case R.id.eyebrows:
                 ImageView eyebrows = (ImageView) findViewById(R.id.eyebrowspic);
                 if (((CheckBox) view).isChecked())
                     eyebrows.setVisibility(View.VISIBLE);
                 else
                     eyebrows.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Ears:
+            case R.id.ears:
                 ImageView ears = (ImageView) findViewById(R.id.earspic);
                 if (((CheckBox) view).isChecked())
                     ears.setVisibility(View.VISIBLE);
                 else
                     ears.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Mouth:
+            case R.id.mouth:
                 ImageView mouth = (ImageView) findViewById(R.id.mouthpic);
                 if (((CheckBox) view).isChecked())
                     mouth.setVisibility(View.VISIBLE);
                 else
                     mouth.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Mustache:
+            case R.id.mustache:
                 ImageView mustache = (ImageView) findViewById(R.id.mustachepic);
                 if (((CheckBox) view).isChecked())
                     mustache.setVisibility(View.VISIBLE);
                 else
                     mustache.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Arms:
+            case R.id.arms:
                 ImageView arms = (ImageView) findViewById(R.id.armspic);
                 if (((CheckBox) view).isChecked())
                     arms.setVisibility(View.VISIBLE);
                 else
                     arms.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Shoes:
+            case R.id.shoes:
                 ImageView shoes = (ImageView) findViewById(R.id.shoespic);
                 if (((CheckBox) view).isChecked())
                     shoes.setVisibility(View.VISIBLE);
                 else
                     shoes.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Glasses:
+            case R.id.glasses:
                 ImageView glasses = (ImageView) findViewById(R.id.glassespic);
                 if (((CheckBox) view).isChecked())
                     glasses.setVisibility(View.VISIBLE);
                 else
                     glasses.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.Hat:
+            case R.id.hat:
                 ImageView hat = (ImageView) findViewById(R.id.hatpic);
                 if (((CheckBox) view).isChecked())
                     hat.setVisibility(View.VISIBLE);
